694024a4841bccbe0dece8d5bfcd632ffdae52bb
craftwerk <pst@crftwrk.de>
2021-04-02 18:49:21 +0200

    jQuery

---
+++ offcanvas-navbar.js
@@ -0,0 +1,20 @@
+
+// Hide offcanvas and backdrop on resize
+$(window).on('resize', function () {
+    $('.offcanvas').offcanvas('hide');
+
+});
+
+
+
+/*
+window.onresize = function () {
+    
+    var element = document.getElementById("offcanvasNavbarExample");
+    element.classList.remove("show");
+
+    document.querySelector('body').classList.remove('offcanvas-backdrop');
+
+}
+*/
+
