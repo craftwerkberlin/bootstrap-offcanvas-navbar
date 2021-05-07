8a35a1f0fc3bbd2cefbf0dbf83ccdcbadc8ec1a1
craftwerk <pst@crftwrk.de>
2021-05-07 13:53:54 +0200

    1.1.4

--- offcanvas-navbar.js
+++ offcanvas-navbar.js
@@ -1,11 +1,4 @@
-// Check if .navbar.fixed-top component exists
-if (document.querySelector('.navbar.fixed-top') !== null) {
-    // window.alert(".navbar.fixed-top exists. Offcanvas backdrop is set to .navbar::before");
-    // Add body class to hide default .offcanvas-backdrop::before if .navbar.fixed-top exists
-    document.body.classList.add("navbar-fixed-top-offcanvas");
-} else {
-    window.alert("No .navbar.fixed-top exists. Offcanvas uses default .offcanvas-backdrop::before body class.");
-}
+
 
 window.onresize = function () {
 // jQuery Workaround, will be replaced by pure JavaScript
--- readme.txt
+++ readme.txt
@@ -2,6 +2,11 @@ Bootstrap navbar Offcanvas Example
 
 == Changelog ==
 
+    = 1.1.4 - 07 May 2021 =
+    
+        * Update Bootstrap 5.0.0
+        * Removed own backdrop and use Bootstrap 5 default 
+
     = 1.1.3 - 08 April 2021 =
     
         * Changed some jQuery with pure JavaScript
